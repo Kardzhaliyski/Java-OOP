29274ad3a1918ca948ca5cd3d91127b717339f33
Zdravko <Zdravko.kardzhaliyski@gmail.com>
2020-05-12 12:27:25 +0300

    Training with Enums

--- 1.WorkingWithAbstractionExercise/src/cardswithpower/Card.java
+++ 1.WorkingWithAbstractionExercise/src/cardswithpower/Card.java
@@ -10,14 +10,14 @@ public class Card {
     }
 
     private int calculatePower() {
-        return rank.getPower();
+        return rank.getPower() + suit.getPower();
     }
 
     @Override
     public String toString() {
         return String.format("Card name: %s of %s; Card power: %d",
-                this.rank,
-                this.suit,
+                this.rank.toString(),
+                this.suit.toString(),
                 this.calculatePower());
     }
 }
